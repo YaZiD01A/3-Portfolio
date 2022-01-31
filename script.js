1b36988398880eb284d29fb24a2a088fe9875af2
Yazid <yalhujery01@gmail.com>
2022-01-31 09:53:26 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  passwordText.value = password;
+  
+  128 > i > 0;
+  prompt()
+
+  
+}
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+  
+
+
+
